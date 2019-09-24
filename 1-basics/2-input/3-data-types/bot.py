47e48671ff52c8df39a511e48a10251687b026f0
WT000 <4TASKW67@solent.ac.uk>
2019-09-24 19:48:25 +0000

    Added new lines

--- 1-basics/2-input/3-data-types/bot.py
+++ 1-basics/2-input/3-data-types/bot.py
@@ -1,15 +1,15 @@
 print("What is your name, human?")
 name = input()
 
-print("How old are you (in years)?")
+print("\nHow old are you (in years)?")
 age = int(input())
 
-print("How tall are you (in meters)?")
+print("\nHow tall are you (in meters)?")
 height = float(input())
 
-print("How much do you weigh (in kilograms)?")
+print("\nHow much do you weigh (in kilograms)?")
 weight = float(input())
 
 bmi = weight / (height ** 2)
 
-print(name + ", you are " + str(age) + " years old and your BMI is " + str(bmi))
+print("\n" + name + ", you are " + str(age) + " years old and your BMI is " + str(bmi))
