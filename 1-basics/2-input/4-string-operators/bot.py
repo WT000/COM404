60517999993c3df33cafe4482654e3362a46686e
WT000 <4TASKW67@solent.ac.uk>
2019-09-24 20:49:19 +0000

    Bot health, energy and shield added

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,16 @@
+print("Please enter the number of lives.")
+lives = int(input())
+print("\nPlease enter the energy level.")
+energy = int(input())
+print("\nPlease enter the shield level.")
+shield = int(input())
+
+print("Health has been set.")
+
+newlives = lives * "♥"
+newenergy = energy * "♦"
+newshield = shield * "♦"
+
+print("Lives:  " + newlives)
+print("Energy: " + newenergy)
+print("Shield: " + newshield)
