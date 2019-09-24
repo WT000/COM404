28fca0fcd69b600830c83e13af2e5775e9033ec1
WT000 <4TASKW67@solent.ac.uk>
2019-09-24 16:45:20 +0000

    BMI in progress

---
+++ 1-basics/2-input/1-user-input/bot.py
@@ -0,0 +1,4 @@
+# Read in user's name
+print("What is your name, human?")
+name = input()
+print("Nice to meet you " + name + ".")
---
+++ 1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,7 @@
+print("What is your name, human?")
+name = input()
+
+print("How old are you (in years)?")
+age = int(input())
+
+print("How tall are you (in meters)?")
