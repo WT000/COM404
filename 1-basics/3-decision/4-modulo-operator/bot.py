55577d86a7d3feb9d1217524f4cb5ca6cdc08353
WT000 <4TASKW67@solent.ac.uk>
2019-10-01 13:53:16 +0000

    Added reference

--- 1-basics/3-decision/4-modulo-operator/bot.py
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -3,6 +3,7 @@ print("Please enter a whole number")
 number = int(input())
 
 # Calculating if the number is even or odd.
+# https://www.w3schools.com/python/python_operators.asp
 if (number % 2 == 0):
     print("The number \"" + str(number) + "\" is an even number.")
 else:
