c45898df3f8e9dc9c3f85078fcf3b0ccd6605a47
WT000 <4TASKW67@solent.ac.uk>
2019-10-01 15:20:31 +0000

    Beep gets scared!

---
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,11 @@
+# Beep hears and sees something
+print("What did I hear?")
+hear = input()
+print("What did I see?")
+see = input()
+
+#Calculation
+if (hear == "grrr") and (see == "two red eyes"):
+    print("There is a scary creature... I SHOULD GET OUT OF HERE!")
+else:
+    print("I am a little scared, but I will continue!")
