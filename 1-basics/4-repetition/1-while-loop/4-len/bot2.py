925c8e7d44cfe2726c31758cdba9a1964506a94c
WT000 <4TASKW67@solent.ac.uk>
2019-10-08 15:25:31 +0000

    Alt method

---
+++ 1-basics/4-repetition/1-while-loop/4-len/bot2.py
@@ -0,0 +1,10 @@
+print("Please enter a phrase")
+entered_phrase = input()
+
+phrase_number = len(entered_phrase)
+bop = 0
+total = 0
+
+while(bop < phrase_number):
+    print("Bop ", end="")
+    bop = bop + 1
