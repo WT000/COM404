71bfe51161c41e55917ec87948527c1a023fda0c
WT000 <4TASKW67@solent.ac.uk>
2019-10-08 14:07:48 +0000

    Used len functions with while

---
+++ 1-basics/4-repetition/1-while-loop/4-len/bot.py
@@ -0,0 +1,10 @@
+# Asking for a phrase
+print("Please enter a phrase:")
+entered_phrase = len(input())
+
+# Control variable
+total_phrase = 0
+
+while (total_phrase < entered_phrase):
+    total_phrase = total_phrase + 1
+    print("Bop ", end="")
