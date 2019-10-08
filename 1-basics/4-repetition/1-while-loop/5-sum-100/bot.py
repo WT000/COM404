1de2513e418933fbf5addccb6b0a69da589e6e54
WT000 <4TASKW67@solent.ac.uk>
2019-10-08 14:47:17 +0000

    Finally did the sum!

---
+++ 1-basics/4-repetition/1-while-loop/5-sum-100/bot.py
@@ -0,0 +1,13 @@
+print("Calculating the sum of the first 100 numbers...")
+
+# Counter
+total = 0
+repeat = 0
+
+# Calculation
+while (repeat < 100):
+    repeat = repeat + 1
+    total = repeat + total
+
+# Total
+print("...Done! The answer is", total)
