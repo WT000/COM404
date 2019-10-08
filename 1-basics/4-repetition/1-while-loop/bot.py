e822c4aac11314574c180fc8f360370503181ccd
WT000 <4TASKW67@solent.ac.uk>
2019-10-08 13:35:55 +0000

    First while loop

---
+++ 1-basics/4-repetition/1-while-loop/bot.py
@@ -0,0 +1,10 @@
+print("How many cables should I remove?")
+maxcables = int(input())
+
+# Control variable
+removedcables = 0
+
+# Cable removal
+while (removedcables < maxcables):
+    removedcables = removedcables + 1
+    print("Removed cable.")
