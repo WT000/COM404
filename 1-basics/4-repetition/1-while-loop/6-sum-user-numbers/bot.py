4e29d6c56fcf2497f877733399db660bf1479a70
WT000 <4TASKW67@solent.ac.uk>
2019-10-08 14:58:39 +0000

    Adding a set amount of numbers the user wants

---
+++ 1-basics/4-repetition/1-while-loop/6-sum-user-numbers/bot.py
@@ -0,0 +1,12 @@
+# Numbers to sum up
+print("How many numbers should I sum up?")
+max_number = int(input())
+number = 0
+add_number = 0
+
+while (number < max_number):
+    print("Please enter number", number, "of", max_number)
+    add_number = int(input()) + add_number
+    number = number + 1
+
+print("The answer is ", add_number, ".")
