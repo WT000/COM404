c4f9567591a1e33ceb3303771b0ade34413de1b0
WT000 <4TASKW67@solent.ac.uk>
2019-10-13 19:50:37 +0000

    Updated code descriptions

--- 1-basics/4-repetition/2-for-loop/2-count-down/bot.py
+++ 1-basics/4-repetition/2-for-loop/2-count-down/bot.py
@@ -1,8 +1,11 @@
+# Asking the user how far we are from the cave
 print("How far are we from the cave?")
 distance = int(input())
 
+# Step loop that minuses by 1
 for count in range (distance, 0, -1):
     print(distance, "steps remaining")
     distance = distance - 1
 
+# End of loop
 print("We have reached the cave!")
