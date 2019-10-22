ef07a018804ab3fb838dc9c532e79d6c908ab491
WT000 <4TASKW67@solent.ac.uk>
2019-10-22 13:30:37 +0000

    Input and Output TCA practice

---
+++ 1-basics/6-tca-practice/2/1-input-output.py
@@ -0,0 +1,8 @@
+# Asking the user what happens when the petal falls
+print("What happens when the last petal falls?")
+
+# Variable
+petal_consequence = input()
+
+# Message with variable
+print("My dear Bella when the last petal falls", petal_consequence)
