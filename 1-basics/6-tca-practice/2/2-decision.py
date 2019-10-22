b43aeaec24b8a4d1fa4787b52a980189e0c6e4e7
WT000 <4TASKW67@solent.ac.uk>
2019-10-22 13:35:34 +0000

    Decision Practice TCA

---
+++ 1-basics/6-tca-practice/2/2-decision.py
@@ -0,0 +1,11 @@
+# Asking if the user has fear in their heart
+print("Have you fear in your heart?")
+
+# User response
+fear = input()
+
+# Response based on the users input
+if (fear == "yes"):
+    print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
+else:
+    print("The force is strong in you. You may be a Jedi apprentice.")
