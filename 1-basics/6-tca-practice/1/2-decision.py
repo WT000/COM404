896cee9e2a718d28e6ef71209f4c4aeba05b072f
WT000 <4TASKW67@solent.ac.uk>
2019-10-22 13:09:31 +0000

    If function code

---
+++ 1-basics/6-tca-practice/1/2-decision.py
@@ -0,0 +1,13 @@
+# Asking the user where Forky is
+print("Where is forky?")
+
+# Variable for users response
+forky_position = input()
+
+# If function with 3 outcomes
+if (forky_position == "With Bonnie"):
+    print("Phew! Bonnie will be happy.")
+elif (forky_position == "Running away"):
+    print("Oh no! Bonnie will be upset!")
+else:
+    print("Ah! I better look for him.")
