2a2f2c58faaaa8d6c84ff4fa30f4c09a60833522
WT000 <4TASKW67@solent.ac.uk>
2019-10-22 13:28:56 +0000

    Added notes

--- 1-basics/6-tca-practice/1/4-function.py
+++ 1-basics/6-tca-practice/1/4-function.py
@@ -1,3 +1,4 @@
+# Suitcase item function
 def item_from_suitcase(item):
     if (item == "toothbrush"):
         print("A toothbrush. Well, got to have clean teeth! \n")
@@ -6,6 +7,7 @@ def item_from_suitcase(item):
     else:
         print("An unexpected item! It could be useful. \n")
 
+# Testing each function variable
 item_from_suitcase("toothbrush")
 item_from_suitcase("belt")
 item_from_suitcase("spidey suit")
