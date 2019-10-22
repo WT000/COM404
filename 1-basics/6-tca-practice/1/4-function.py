87a70ab481d0fffe2d6436c4c7213bd8fda384be
WT000 <4TASKW67@solent.ac.uk>
2019-10-22 13:26:43 +0000

    Practice TCA function

---
+++ 1-basics/6-tca-practice/1/4-function.py
@@ -0,0 +1,11 @@
+def item_from_suitcase(item):
+    if (item == "toothbrush"):
+        print("A toothbrush. Well, got to have clean teeth! \n")
+    elif (item == "spidey suit"):
+        print("My Spidey suit! I won't be needing this. I am on holiday. \n")
+    else:
+        print("An unexpected item! It could be useful. \n")
+
+item_from_suitcase("toothbrush")
+item_from_suitcase("belt")
+item_from_suitcase("spidey suit")
