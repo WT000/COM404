ad6fa2adb0d2b2a2813c165ac0ed0738d1f1b77a
WT000 <4TASKW67@solent.ac.uk>
2019-11-05 16:19:46 +0000

    Added access modifiers

--- 2-guis/1-classes-and-objects/FlyingBot.py
+++ 2-guis/1-classes-and-objects/FlyingBot.py
@@ -4,7 +4,7 @@ from SuperBot import SuperBot
 class FlyingBot(SuperBot):
     def __init__(self, name, age, energy, shield, super_power_level, hover):
         super().__init__(name, age, energy, shield, super_power_level)
-        self.hover = hover
+        self.__hover = hover
 
     def display_hover(self):
-        print(self.hover)
+        print(self.__hover)
--- 2-guis/1-classes-and-objects/SuperBot.py
+++ 2-guis/1-classes-and-objects/SuperBot.py
@@ -4,7 +4,7 @@ from Bot import Bot
 class SuperBot(Bot):
     def __init__(self, name, age, energy, shield, super_power_level):
         super().__init__(name, age, energy, shield)
-        self.super_power_level = super_power_level
+        self.__super_power_level = super_power_level
 
     def display_super_power_level(self):
-        print(self.super_power_level)
+        print(self.__super_power_level)
