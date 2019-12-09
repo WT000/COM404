fed1b3c09dbc88beaa4f5c3ca425420bfe071295
WT000 <4taskw67@solent.ac.uk>
2019-12-09 13:40:08 +0000

    Coded walking animation with varying ticks

--- 2-guis/5-animations/3-varying-ticks/main.py
+++ 2-guis/5-animations/3-varying-ticks/main.py
@@ -7,11 +7,11 @@ class Gui(Tk):
         super().__init__()
         
         # Load resources
-        self.redman_image = PhotoImage(file="U:/com404/COM404/2-guis/5-animations/3-varying-ticks/redman.gif")
-        self.redmanleft_image = PhotoImage(file="U:/com404/COM404/2-guis/5-animations/3-varying-ticks/redmanleft.gif")
+        self.redman_image = PhotoImage(file="C:/Users/Will/Desktop/COM404/COM404/2-guis/5-animations/3-varying-ticks/redman.gif")
+        self.redmanleft_image = PhotoImage(file="C:/Users/Will/Desktop/COM404/COM404/2-guis/5-animations/3-varying-ticks/redmanleft.gif")
 
-        self.blueman_image = PhotoImage(file="U:/com404/COM404/2-guis/5-animations/3-varying-ticks/blueman.gif")
-        self.bluemanleft_image = PhotoImage(file="U:/com404/COM404/2-guis/5-animations/3-varying-ticks/bluemanleft.gif")
+        self.blueman_image = PhotoImage(file="C:/Users/Will/Desktop/COM404/COM404/2-guis/5-animations/3-varying-ticks/blueman.gif")
+        self.bluemanleft_image = PhotoImage(file="C:/Users/Will/Desktop/COM404/COM404/2-guis/5-animations/3-varying-ticks/bluemanleft.gif")
         
         # Window attributes
         self.title("Walking Animation")
@@ -29,35 +29,47 @@ class Gui(Tk):
         self.__redman_image_label()
         self.__blueman_image_label()
 
+        # Tick number
+        self.__num_tick = 0
+        
         # Start the timer
-        self.__num_tick
         self.__tick()
 
     def __tick(self):
-        # -----------------------------------------------------------
-        self.redman_x_pos = self.redman_x_pos + self.redman_x_change
-        if (self.redman_x_pos > 220):
-            self.redman_x_change = -1
-            self.redman_image_label.configure(image=self.redmanleft_image)
-            
-        elif (self.redman_x_pos < 0):
-            self.redman_x_change = 1
-            self.redman_image_label.configure(image=self.redman_image)
-
-        self.redman_image_label.place(x=self.redman_x_pos)
-        # -----------------------------------------------------------
-        self.blueman_x_pos = self.blueman_x_pos + self.blueman_x_change
-        if (self.blueman_x_pos < 0):
-            self.blueman_x_change = 1
-            self.blueman_image_label.configure(image=self.blueman_image)
+        # Tick counter
+        self.__num_tick = self.__num_tick + 1
         
-        elif (self.blueman_x_pos > 220):
-            self.blueman_x_change = -1
-            self.blueman_image_label.configure(image=self.bluemanleft_image)
-
-        self.blueman_image_label.place(x=self.blueman_x_pos)
+        # Redman
         # -----------------------------------------------------------
-        self.after(10, self.__tick)
+        if (self.__num_tick % 2 == 0):
+        
+            if (self.redman_x_pos < 0):
+                self.redman_x_change = 1
+                self.redman_image_label.configure(image=self.redman_image)
+        
+            elif (self.redman_x_pos > 220):
+                self.redman_x_change = -1
+                self.redman_image_label.configure(image=self.redmanleft_image)
+
+            self.redman_x_pos = self.redman_x_pos + self.redman_x_change
+            self.redman_image_label.place(x=self.redman_x_pos)
+        
+        # Blueman
+        # -----------------------------------------------------------
+        if (self.__num_tick % 4 == 0):
+        
+            if (self.blueman_x_pos < 0):
+                self.blueman_x_change = 1
+                self.blueman_image_label.configure(image=self.blueman_image)
+        
+            elif (self.blueman_x_pos > 220):
+                self.blueman_x_change = -1
+                self.blueman_image_label.configure(image=self.bluemanleft_image)
+
+            self.blueman_x_pos = self.blueman_x_pos + self.blueman_x_change
+            self.blueman_image_label.place(x=self.blueman_x_pos)
+        # -----------------------------------------------------------
+        self.after(5, self.__tick)
     
     def __redman_image_label(self):
         self.redman_image_label = Label()
